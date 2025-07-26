4ddc3e3b773fb781996f8bfde27ab9c4c2f0e9f1
Kelly Coldren <shrtpvteacher@protonmail.com>
2025-07-26 14:50:08 -0700

    adjusted background in AboutMePage.jsx

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -5,7 +5,12 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div className="min-h-screen text-gray-200 px-6 py-12">
+       <div 
+          className="w-full bg-cover bg-center  text-gray-200 py-16 px-6"
+          style={{ backgroundImage: `url(${VectorBg})` }}
+       
+    >
+         
       <div className="max-w-3xl mx-auto">
         <h1 className="text-4xl font-bold mb-8 text-purple-300">About Me</h1>
 
