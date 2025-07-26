80f935b130b92ce1f2f8b3d5339c98c8c345734b
Kelly Coldren <shrtpvteacher@protonmail.com>
2025-07-26 14:46:19 -0700

    imported AboutMe page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PortfolioPage from "./pages/PortfolioPage";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from "./components/Footer.jsx";
+import AboutMe from "./pages/AboutMePage.jsx";
 
 
 // import more pages as needed
